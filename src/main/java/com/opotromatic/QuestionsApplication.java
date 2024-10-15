cc31113c62bdad0bae6b8abb1970dc616bd676e3
Enrique NG <enrique.naranjo.guerra@gmail.com>
2024-10-15 22:53:17 +0200

    tyleaf templates added

--- src/main/java/com/opotromatic/QuestionsApplication.java
+++ src/main/java/com/opotromatic/QuestionsApplication.java
@@ -1,9 +1,13 @@
 package com.opotromatic;
 
+import jakarta.annotation.PostConstruct;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+
+import java.awt.*;
+import java.net.URI;
 
 @EnableJpaRepositories("com.opotromatic.repositories")
 @EntityScan("com.opotromatic.entities")
@@ -13,5 +17,14 @@ public class QuestionsApplication {
     public static void main(String[] args) {
         SpringApplication.run(QuestionsApplication.class, args);
     }
+
+//    @PostConstruct
+//    public void openBrowser(){
+//        try{
+//            Desktop.getDesktop().browse(new URI("http://localhost:8080/index"));
+//        } catch (Exception e){
+//            e.printStackTrace();
+//        }
+//    }
 
 }
